8347508b402a46377fd7dca01d26f3efe2b0aa75
Mads Hornbeck <madshornbeck@gmail.com>
2021-04-29 16:45:58 +0200

    Fix react peer dependency version

--- example/src/index.js
+++ example/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "@hornbeck/react-modals/style.css";
 import App from "./App";
 import { ModalProvider } from "@hornbeck/react-modals";
 
